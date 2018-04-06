9885a35e394abf0669485e77b0cfb8134c968ccc
Pigareva Oksana <pigarevaoks@gmail.com>
2018-04-06 17:17:39 +0300

    upd: Range Slider

--- src/components/RangeSlider/RangeSlider.js
+++ src/components/RangeSlider/RangeSlider.js
@@ -2,11 +2,33 @@ import React from 'react'
 import styles from './RangeSlider.css';
 import classNames from 'classnames';
 
-const RangeSlider = ({ layout, name, percent, index, onChange }) => (
-    <div className={classNames([styles.container, layout])}>
-        <span className={styles.title}>{name}</span>
-        <input className={styles.slider} type="range" min="0" max="100" step="0.01" value={percent} onChange={e => onChange(index, e.target.value)} />
-    </div>
-);
+class RangeSlider extends React.Component {
+
+    render() {
+        const { layout, name, percent, index, onChange  } = this.props;
+        return(
+            <div className={classNames([styles.container, layout])}>
+                <span className={styles.title}>{name}</span>
+                <input className={styles.slider} 
+                    type="range" 
+                    min="0" 
+                    max="100" 
+                    step="0.01" 
+                    value={percent} 
+                    onChange={e => onChange(index, e.target.value)} 
+                />
+                <input className={styles.input}
+                    type="number"
+                    min="0"
+                    max="100"
+                    step="1"
+                    value={percent}
+                    onChange={e => onChange(index, e.target.value)} 
+                />
+            </div>
+        );
+
+    }
+}
 
 export default RangeSlider;
